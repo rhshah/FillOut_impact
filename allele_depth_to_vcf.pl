cb0d31e5d02b5f282ffef37f7714eb5b3a214cf6
GowthamJ <andurill@users.noreply.github.com>
2016-04-29 16:22:44 -0400

    Update allele_depth_to_vcf.pl
    v1.1 - Added support for batch processing and the option to handle either qsub or bsub

--- allele_depth_to_vcf.pl
+++ allele_depth_to_vcf.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl
 
 # allele_depth_to_vcf - Reformat and merge variants from multiple alleledepth files into a single vcf.
-# version: v1.0
+# version: v1.1
 # Author: Gowtham Jayakumaran ( jayakumg <at> mskcc <dot> org )
 # Date: 25/Apr/2016
-# Last Modified: 25/Apr/2016
+# Last Modified: 28/Apr/2016
+# Modification timeline ( for major changes )
+# 28/Apr/2016 --- v1.1 - Added support for batch processing and the option to handle either qsub or bsub
 
 use strict;
 use warnings;
@@ -13,62 +15,71 @@ use Getopt::Long qw( GetOptions );
 use Pod::Usage qw( pod2usage );
 use feature qw( say );
 use Cwd qw( abs_path );
+#use Data::Dumper;
+#use MSKCC_DMP_Logger;
 
 
 # parse options and print error, if any, or print usage, if help or man requested.
-my( $help, $man, $keep, $i_vcf, $g_allele, $samtools, $i_bam, $o_dir, $o_vcf, $qsub );
+my( $help, $man, $remove, $i_vcf, $g_allele, $samtools, $bedtools, $RefFile, $i_bam, $o_dir, $o_vcf, $perl, $qsub, $bsub, $queue );
 if( @ARGV < 1 or
 	$ARGV[0] !~ m/^-/ or
 	!GetOptions(
 		'help!' => \$help,
 		'man!' => \$man,
-		'keep!' => \$keep,
+		'remove!' => \$remove,
 		'i_vcf=s' => \$i_vcf,
 		'g_allele=s' => \$g_allele,
 		'samtools=s' => \$samtools,
+		'bedtools=s' => \$bedtools,
+		'RefFile=s' => \$RefFile,
 		'i_bam=s' => \$i_bam,
 		'o_dir=s' => \$o_dir,
 		'o_vcf=s' => \$o_vcf,
-		'qsub=s' => \$qsub
+		'perl=s' => \$perl,
+		'qsub=s' => \$qsub,
+		'bsub=s' => \$bsub,
+		'queue=s' => \$queue
 		) )	{
 	pod2usage( -verbose => 1, -input => \*DATA, -exitval => 2 );
 }
 pod2usage( -verbose => 1, -input => \*DATA, -exitval => 0 ) if( $help );
 pod2usage( -verbose => 2, -input => \*DATA, -exitval => 0 ) if( $man );
 
+# obtain absolute paths for the input and output files
+for my $file ( $i_vcf, $g_allele, $samtools, $i_bam, $o_dir, $o_vcf, $perl, $qsub, $bsub )	{
+	$file = abs_path( $file ) if defined $file;
+}
+
 # set default paths and constants
-$g_allele = "/dmp/resources/prod/software/dmp-impact-res/VERSIONS/dmp-impact-res-v118/bin/dmp_genotype_allele.pl" unless defined $g_allele and ( -e $g_allele );
+$g_allele = "/dmp/resources/prod/software/dmp-impact-res/VERSIONS/dmp-impact-res-v118/bin/dmp_genotype_allele.pl" unless defined $g_allele;
 $samtools = "/dmp/resources/prod/tools/bio/samtools/production/samtools" unless defined $samtools;
-$qsub = "/common/sge/bin/lx-amd64/qsub" unless defined $qsub;
-$samtools = `which samtools` unless defined $samtools and ( -e $samtools );
-chomp ( $samtools );
-( $samtools and -e $samtools ) or die "ERROR: Please install samtools and include it in your PATH.\n";
-$qsub = `which qsub` unless defined $qsub and ( -e $qsub );
-chomp ( $qsub );
-( $qsub and -e $qsub ) or die "ERROR: Please provide the path to qsub or include it in your PATH.\n";
+$perl = "$^X" unless defined $perl and ( -e $perl ); # /usr/bin/env perl
+$bedtools = "/home/shahr2/Software/BEDTools/current/bin/" unless defined $bedtools;
+$RefFile = "/home/shahr2/Resources/IMPACT/References/Homo_sapiens_assembly19_formatted.fasta" unless defined $RefFile;
 
-die "ERROR: Please provide the path for your genotype script. Try perl $0 --help for more info.\n" unless ( -e $g_allele ) ;
-die "ERROR: Please provide a vcf file. Try perl $0 --help for more info.\n" unless defined $i_vcf and ( -e $i_vcf ) ;
-die "ERROR: Please provide a proper argument for --i_bam. Try perl $0 --help for more info.\n" unless defined $i_bam and ( -e $i_bam ) ;
+die "ERROR: Please provide either --qsub or --bsub, and not both.\n" if ( ( $qsub and $bsub ) or ( !$qsub and !$bsub ) );
+die "ERROR: Please provide the -queue on which you want this program to run.\n" unless defined $queue;
+die "ERROR: Default genotype script does not exist. Please provide the path for your genotype script.\n" unless ( -e $g_allele );
+die "ERROR: Please provide a vcf file.\n" unless defined $i_vcf and ( -e $i_vcf );
+die "ERROR: Please provide a proper argument for --i_bam.\n" unless defined $i_bam and ( -e $i_bam );
+die "ERROR: Please provide the path to samtools.\n" unless defined $samtools and ( -e $samtools );
+die "ERROR: Please provide the path to bedtools.\n" unless defined $bedtools and ( -e $bedtools );
+die "ERROR: Please provide the path to a reference genome hg19.fasta.\n" unless defined $RefFile and ( -e $RefFile );
+die "ERROR: Please provide the path to perl.\n" unless defined $perl and ( -e $perl );
 
-# set output dir and files
+# set output dir
 $o_dir = `pwd` unless defined $o_dir and ( -e $o_dir );
 chomp $o_dir;
 
-# set output .vcf file
+# set output .vcf file. To be standardized later
 if( defined $o_vcf )	{
-	$o_vcf =~ s/(\.)?(maf|tsv|csv|txt)?$/.vcf/;
+	map{ s/(.+\/)?/$o_dir\//; s/(\.)?(maf|tsv|csv|txt)?$/.vcf/ } $o_vcf;
 	$o_vcf .= ".vcf" unless( $o_vcf =~ m/(\.)?(vcf)?$/ );
 }else	{
 	$o_vcf = $o_dir."/allele_depth_merged.vcf";
 }
 
-# obtain absolute paths for all the input and output files
-for my $file ( $g_allele, $i_vcf, $o_vcf, $i_bam )	{
-	$file = abs_path( $file );
-}
-
-# parse the arguments to --i_bam depending on whether its a comma-separated string of bam file paths or a text file with bam file paths;
+# parse the arguments to --i_bam depending on whether its a comma-separated string of bam file paths or a text file with bam file paths
 my @bam = ();
 if ($i_bam =~ /.txt$/ )	{
 	my $bam_file = IO::File->new( $i_bam ) or die "ERROR: Cannot open file $i_bam\n";
@@ -85,16 +96,16 @@ if ($i_bam =~ /.txt$/ )	{
 # check whether the vcf file provided is in the proper format. A more stringent check can be implemented, if necessary.
 my $vcf_header = `grep '^#' $i_vcf`;
 $vcf_header =~ s/\n//g;
-map{ ( m/^\#\#fileformat=VCFv4.2.+\#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT/ ) or 
+map{ ( m/^\#\#fileformat=VCFv4.2.+\#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO/ ) or 
 	die "ERROR: $i_vcf is not in the vcf4.2 format. Please check https://samtools.github.io/hts-specs/VCFv4.2.pdf for more info.\n" } $vcf_header;
 
 # Extracting sample names from bam file, to be included in the VCF
-my @bam_sample_name = ();
+my @bam_sample_names = ();
 foreach my $bam_file ( @bam )	{
 	die "ERROR: $bam_file does not exist.\n" unless ( -e $bam_file );
 	my $header = `$samtools view -H $bam_file`;
 	map{ s/\n//g; s/.+\@RG\sID:(\S+)\s.+/$1/; } $header;
-	push @bam_sample_name, $header;
+	push @bam_sample_names, $header;
 }
 
 # get the absolute path and bam file names of all mpileup.alleledepth output files in $o_dir
@@ -107,11 +118,24 @@ map{ s/(.+\/)?//; s/(\.bam)?$/_mpileup.alleledepth/; } @alleledepth_file_names;
 # run dmp_genotype_allele.pl on each of the input bam files and direct the ouput mpileup.alleledepth files to $o_dir
 while ( my $bam_file = shift @bam )	{
 	die "ERROR: $bam_file does not exist.\n" unless ( -e $bam_file );
-	say "Genotyping $bam_file ...";
-	my $exitstatus = system ( "/usr/bin/env perl $g_allele --outdir $o_dir -fmv $i_vcf -bam $bam_file --qsub $qsub -q test.q 1>/dev/null" );
-	die "ERROR: $g_allele did not execute successfully. Please check your arguments and path of your input files.\n" unless( $exitstatus == 0 );
+	map{ s/(.+\/)?//; s/(\.bam)?$//; } ( my $job_name = $bam_file );
+	#say "Genotyping $bam_file ...";
+	#my $exitstatus = system ( "/usr/bin/env perl $g_allele --outdir $o_dir -fmv $i_vcf -bam $bam_file --qsub $qsub -q $queue 1>/dev/null" );
+	if ( $qsub )	{
+		`$qsub -q $queue -V -N adepth2vcf_$job_name -wd $o_dir -e adepth2vcf.$job_name.stderr -o adepth2vcf.$job_name.stdout -l h_vmem=2G,virtual_free=2G -pe smp 1 -b y $perl $g_allele -o $o_dir -fmv $i_vcf -bam $bam_file -rf $RefFile -s $samtools -o $o_dir -b $bedtools -qsub $qsub -q $queue`; # default parameters for $g_allele script that will de added later: [ -of $alleleDepthMutationOut -mof $mpileUpMutationOut -bi $bamID -mmq $MAPQ -mbq $BASQ ]
+		}else	{
+			`$bsub -q $queue -J adepth2vcf_$job_name -cwd $o_dir -e adepth2vcf.$job_name.stderr -o adepth2vcf.$job_name.stdout -We 24:00 -R "rusage[mem=2]" -M 4 -n 1 "$perl $g_allele -fmv $i_vcf -bam $bam_file -rf $RefFile -s $samtools -o $o_dir -q $queue -b $bedtools -bsub $bsub"`; # default parameters for $g_allele script will be added later: [ -of $alleleDepthMutationOut -mof $mpileUpMutationOut -bi $bamID -mmq $MAPQ -mbq $BASQ ]
+		}
+	#die "ERROR: $g_allele did not execute successfully. Please check your arguments and path of your input files.\n" unless( $exitstatus == 0 );
 }
 
+# To wait for all the jobs genotyping jobs submitted to LSF/SGE to complete before proceeding to the next step
+my @stdout_files = ();
+until ( scalar @stdout_files == scalar @bam_sample_names )	{
+	@stdout_files = `ls $o_dir | grep "_mpileup.alleledepth\$"`;
+	map{ s/^\s+|\s+$|\r|\n//g; $_ } @stdout_files;
+	sleep ( 10 );
+}
 
 # parse all the mpileup.alleledepth files in $o_dir and merge the output into a single VCF
 my $vcf_out = IO::File->new( $o_vcf, ">" ) or die "ERROR: Failed to create file $o_vcf\n";
@@ -129,7 +153,7 @@ $vcf_out->print( "##FORMAT=<ID=RDN,Number=1,Type=Integer,Description=\"Reference
 $vcf_out->print( "##FORMAT=<ID=ADP,Number=1,Type=Integer,Description=\"Alternate depth on postitive strand\">\n" );
 $vcf_out->print( "##FORMAT=<ID=ADN,Number=1,Type=Integer,Description=\"Alternate depth on negative strand\">\n" );
 $vcf_out->print( "##FILE_TYPE=<analysis_type=merged_mpileup.alleledepth_files input_file=[".join( ", ", @alleledepth_file_names )."]>\n" );
-$vcf_out->print( "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t".join( "\t", @bam_sample_name )."\n");
+$vcf_out->print( "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t".join( "\t", @bam_sample_names )."\n");
 
 my %variant_metrics = (); # hash of all unique variants and their coordinates as keys and their correspoding allelic info as values
 while( my $ad_file_path = shift @alleledepth_files )	{
@@ -138,14 +162,14 @@ while( my $ad_file_path = shift @alleledepth_files )	{
 	my $ad_file = IO::File->new( $ad_file_path ) or die "ERROR: Cannot open file: $ad_file_path\n";
 	chomp( my $header = $ad_file->getline );
 	die "ERROR: $ad_file is not in the proper mpileup.alleledepth file format.\n" unless ( $header =~ m/^Ref_BAM\tSample\tChrom\tPOS\tRef\tAlt\tTotal_Depth\tRef_Depth\tAlt_Counts\tAlt_Freq\tRef_Forward\tRef_Reverse\tAlt_Forward\tAlt_Reverse$/ );
-	say "Parsing $ad_file_path ...";
+	#say "Parsing $ad_file_path ...";
 	while( my $line = $ad_file->getline )	{
 		chomp $line;
 		my( $variant_info, $allelic_info ) = ad_to_vcf( $line );
 		( $variant_metrics{$variant_info} ) ? ( $variant_metrics{$variant_info} .= "\t$allelic_info" ) : ( $variant_metrics{$variant_info} = "$allelic_info" );
 	}
 	$ad_file->close;
-	system( "rm $ad_file_path $o_dir/*stderr $o_dir/*stat $o_dir/Notify.csh $o_dir/Impact_local.log 1>/dev/null 2>/dev/null" ) unless defined $keep;
+	system( "rm $ad_file_path $o_dir/*stderr $o_dir/*stat $o_dir/Notify.csh $o_dir/Impact_local.log 1>/dev/null 2>/dev/null" ) if defined $remove;
 }
 
 my $allelic_format = qw( DP:RD:AD:VF:DPP:DPN:RDP:RDN:ADP:ADN );
@@ -181,7 +205,13 @@ allele_depth_to_vcf - Reformat and merge variants from multiple alleledepth file
 =head1 SYNOPSIS
 
  perl allele_depth_to_vcf.pl --help
- perl allele_depth_to_vcf.pl --i_bam list_of_bams_files.txt --i_vcf file.vcf
+ perl allele_depth_to_vcf.pl --i_bam list_of_bams_files.txt --i_vcf file.vcf --qsub /common/sge/bin/lx-amd64/qsub --queue test.q
+
+ Sample call for SGE:
+ qsub -q test.q -V -N jobname -wd working_dir_path -e jobname.stderr -o jobname.stdout -l h_vmem=2G,virtual_free=2G -pe smp 1 -b y perl allele_depth_to_vcf.pl --i_bam my_test_bams.txt --i_vcf input.vcf --o_dir output_dir_path --qsub qsub_executable_path --queue example.q --o_vcf name_for_output_vcf
+ 
+ Sample call for LSF:
+ bsub -q examplq_queue -J jobname -cwd working_dir_path -e jobname.stderr -o jobname.stdout -We 02:00 -R "rusage[mem=2]" -M 4 -n 1 "perl allele_depth_to_vcf.pl --i_vcf input.vcf --i_bam files_of_bam_paths.txt --bsub bsub_executable_path --queue example_queue --o_dir output_dir_path --o_vcf name_for_output_vcf"
 
 =head1 OPTIONS
 
@@ -190,9 +220,14 @@ allele_depth_to_vcf - Reformat and merge variants from multiple alleledepth file
  --o_vcf          Path to output multi-sample VCF [<o_dir>/allele_depth_merged.vcf] (optional)
  --g_allele       Path to dmp_genotype_allele.pl script DMP IMPACT pipeline [/dmp/resources/prod/software/dmp-impact-res/production/bin/dmp_genotype_allele.pl] (optional)
  --samtools       Path to samtools [/dmp/resources/prod/tools/bio/samtools/production/samtools] (optional)
- --qsub           Path to qsub [/common/sge/bin/lx-amd64/qsub] (optional)
+ --bedtools       Path to bedtools [/home/shahr2/Software/BEDTools/current/bin/] (optional)
+ --perl           Path to the perl binary that you want to use to run this program [/usr/bin/env perl] (optional)
+ --qsub           Path to qsub executable for SGE (required if --bsub is not specified)
+ --bsub           Path to qsub executable for LSF (required if --qsub is not specified)
+ --queue          Name of the SGE / LSF queue where this software should run (required)
  --i_bam          Comma-separated absolute paths to bam files or a text file, with a .txt suffix, of all the absolute paths to bam files with line breaks (required)
- --keep           Keep intermediate mpileup.alledepth and other files (optional)
+ --RefFile        Path to reference genome file [/home/shahr2/Resources/IMPACT/References/Homo_sapiens_assembly19_formatted.fasta] (optional)
+ --remove         Remove intermediate mpileup.alledepth and other files (optional)
  --help           Print help message and quit (optional)
  --man            Print detailed message and usage instructions and quit (optional)
 
